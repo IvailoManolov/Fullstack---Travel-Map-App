0cb43b3bdb5eb25df184811e57a18bc66e7b0264
Ivaylo <ivomoreras@abv.bg>
2022-11-10 20:38:09 +0200

    Created user and pin schemas

---
+++ back-end/models/Pin.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose")
+
+const PinSchema = new mongoose.Schema({
+    userName : {
+        type : String,
+        require : true
+    },
+    title : {
+        type : String,
+        require : true,
+        min : 3
+    },
+    rating : {
+        type : Number,
+        require : true,
+        min : 0,
+        max : 5
+    },
+
+    lat : {
+        type : Number,
+        require : true,
+    },
+    lon : {
+        type : Number,
+        require : true
+    }
+},{timestamps : true})
+
+module.exports = mongoose.model("Pin", PinSchema)
