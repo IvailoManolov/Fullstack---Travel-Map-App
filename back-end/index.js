2a5557772256058dc9e59ae29ed9fe529cf20bcf
Ivaylo <ivomoreras@abv.bg>
2022-11-12 19:18:03 +0200

    finished user login/registering api

--- back-end/index.js
+++ back-end/index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const mongoose = require("mongoose")
 const dotEnv = require("dotenv")
 const pinRoute = require("./routes/pins")
+const userRoute = require("./routes/users")
 
 dotEnv.config()
 
@@ -15,6 +16,7 @@ mongoose.connect(process.env.MONGO_URL).then(() => {
 .catch((err) => console.log('\x1b[41m%s\x1b[0m',"[FAILED]Mongo DB failed to connect" + err))
 
 application.use("/api/pins",pinRoute)
+application.use("/api/users",userRoute)
 
 application.listen(8800,() => {
     console.log(process.env.MONGO_URL)
