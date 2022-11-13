9e0e48abdd4716ceee2ccd4a4c0768012ad24bb9
Ivaylo <ivomoreras@abv.bg>
2022-11-13 11:25:09 +0200

    upstiling the globe map

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,24 +1,30 @@
 import * as React from 'react';
 import Map, {Marker} from 'react-map-gl';
 import FmdGoodIcon from '@mui/icons-material/FmdGood';
+import {useState} from "react"
 import "./App.css"
 import 'mapbox-gl/dist/mapbox-gl.css'
 
 function App() {
+
+  const [viewPort,setViewPort] = useState({
+    longitude: -122.4,
+    latitude: 37.8,
+    zoom: 14
+  })
+
   return (
     <div className='app'>
     <Map
-      initialViewState={{
-        longitude: 17,
-        latitude: 46,
-        zoom: 4
-      }}
+      container ={'map'}
+      projection={'globe'}
+      initialViewState={{viewPort}}
       mapboxAccessToken={process.env.REACT_APP_MAPBOX}
       style={{width: "100vw", height: "100vh"}}
-      mapStyle="mapbox://styles/mapbox/streets-v9">
+      mapStyle="mapbox://styles/ivomoreras/claf55gfo005114qwe1aed26r">
 
-      <Marker longitude={1.8566} latitude={48.2946}  anchor="center">
-        <FmdGoodIcon />
+      <Marker longitude={2.3566} latitude={48.7946}  anchor="center">
+        <FmdGoodIcon style ={{fontSize : viewPort.zoom * 2, color:"slateblue"}}/>
       </Marker>
     </Map>
     </div>
